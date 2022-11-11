fe6e9bfc581212dc9bd2a99df16e537935828eae
Mathias Thibaud <mathias.thibaud@visualping.io>
2022-11-10 17:39:53 -0800

    style: add bg color

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen bg-yellow-400">
       <div className="m-auto text-center">
         <h1 className="text-3xl font-bold">🚧 UNDER CONSTRUCTION 🚧</h1>
-        <h2 className="mt-4">We are currently working on this page</h2>
+        <h2 className="mt-6 text-xl">We are currently working on this page</h2>
       </div>
     </div>
   );
