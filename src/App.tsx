d553ef0490b484e355512777da5c37adae0953ed
Mathias Thibaud <mathias.thibaud@visualping.io>
2022-11-10 17:54:39 -0800

    feat: create UnderConstruction component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import "./App.css";
+import UnderConstruction from "./pages/UnderConstruction";
 
 function App() {
-  return (
-    <div className="flex h-screen bg-yellow-400">
-      <div className="m-auto text-center">
-        <h1 className="text-3xl font-bold">🚧 UNDER CONSTRUCTION 🚧</h1>
-        <h2 className="mt-6 text-xl">We are currently working on this page</h2>
-      </div>
-    </div>
-  );
+  return <UnderConstruction />;
 }
 
 export default App;
---
+++ src/pages/UnderConstruction.tsx
@@ -0,0 +1,12 @@
+const UnderConstruction = () => {
+  return (
+    <div className="flex h-screen bg-yellow-400">
+      <div className="m-auto text-center">
+        <h1 className="text-3xl font-bold">🚧 UNDER CONSTRUCTION 🚧</h1>
+        <h2 className="mt-6 text-xl">We are currently working on this page</h2>
+      </div>
+    </div>
+  );
+};
+
+export default UnderConstruction;
