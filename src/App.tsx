a1cb86c13fe1cf413b5fc119136cc1e3f4d8408f
Mathias Thibaud <mathias.thibaud@visualping.io>
2022-11-16 17:38:00 -0800

    style: edit UnderConstruction

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Footer from "./components/Footer";
 import UnderConstruction from "./pages/UnderConstruction";
 
 function App() {
-  return <UnderConstruction />;
+  return (
+    <div className="flex flex-col h-screen bg-yellow-400">
+      <UnderConstruction />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/UnderConstruction.tsx
+++ src/pages/UnderConstruction.tsx
@@ -1,22 +1,25 @@
 const UnderConstruction = () => {
   return (
-    <div className="flex flex-col h-screen bg-yellow-400">
-      <header className="py-5 text-center text-sm md:text-base">
-        mathiasthibaud.com
-      </header>
-      <main className="flex-1 flex items-center justify-center overflow-y-auto text-center p-5">
-        <div>
-          <h1 className="text-xl md:text-3xl font-bold">
-            🚧 UNDER CONSTRUCTION 🚧
-          </h1>
-          <h2 className="mt-6 text-base md:text-xl">
-            We are currently working on this page
-          </h2>
+    // <main className="flex-1 flex items-center justify-center overflow-y-auto text-center p-5">
+    //   <div className="">
+    //     <h1 className="text-3xl sm:text-4xl font-bold">
+    //       🚧 UNDER CONSTRUCTION 🚧
+    //     </h1>
+    //     <h2 className="mt-6 text-lg">We are currently working on this page</h2>
+    //   </div>
+    // </main>
+    <div className="bg-yellow-400 flex-1 flex items-center justify-center">
+      <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-lg font-semibold text-teal-800">Coming Soon</h2>
+          <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
+            Under construction.
+          </p>
+          <p className="mx-auto mt-5 max-w-xl text-xl text-gray-500">
+            We are currently working on the new website.
+          </p>
         </div>
-      </main>
-      <footer className="py-5 text-center bg-teal-800 text-white text-sm md:text-base">
-        Designed & Built by Mathias Thibaud{" "}
-      </footer>
+      </div>
     </div>
   );
 };
