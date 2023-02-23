3aea45c8245e01459c3f9afae1e20aa1a7db0e68
Mathias Thibaud <mathias.thibaud@visualping.io>
2023-02-23 10:04:01 -0800

    style: edit footer text color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -35,7 +35,7 @@ function Footer() {
   ];
 
   return (
-    <footer className="bg-teal-800 text-gray-400">
+    <footer className="bg-teal-800 text-gray-300">
       <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
